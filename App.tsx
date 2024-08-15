a5c99e89b6d9b8df72143ec234a3e48dc81cfe6b
unknown <clara.silva72@etec.sp.gov.br>
2024-08-15 14:08:56 -0300

    15/08

--- App.tsx
+++ App.tsx
@@ -1,13 +1,25 @@
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, TouchableOpacity ,View, Image } from 'react-native';
 import ligado from './assets/pictures/symbol-on.png';
 import desligado from './assets/pictures/symbol-off.png';
+import React, {useState} from 'react';
+
+
 
 
 export default function App() {
-  let isActive = true;
+  const[isActive, setisActive]= useState(false);  
+
+  function handleSymbol(){
+    setisActive((oldValue:boolean)=>{
+      return !oldValue;
+    })
+  }
   return (
   <View style={isActive ? styles.containerOn : styles.containerOff}>
-      <Image source={isActive ? ligado : desligado}></Image>
+    <TouchableOpacity onPress={handleSymbol}>
+    <Image source={isActive ? ligado : desligado}></Image>
+    </TouchableOpacity>
+     
   </View>
   );
 }
@@ -15,13 +27,13 @@ export default function App() {
 const styles = StyleSheet.create({
   containerOn: {
     flex: 1,
-    backgroundColor: '#000000',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor:'#000000',
+    alignItems:'center',
+    justifyContent:'center',
   },
   containerOff: {
     flex: 1,
-    backgroundColor: '#fffggggggggggggg',
+    backgroundColor:'#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
